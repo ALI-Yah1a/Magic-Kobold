d3420faafd8726dfb9993b356b339849356da0c8
nourtamerhamdyabbas-ux <nourtamerhamdyabbas@gmail.com>
2026-09-20 20:37:34 +0300

    giving the last edit on the options menu and decorating it with testing

--- Scenes/options.gd
+++ Scenes/options.gd
@@ -28,6 +28,8 @@ func _on_music_volume_slider_value_changed(value: float) -> void:
 
 
 func _on_fullscreen_button_toggled(toggled_on: bool) -> void:
+	print("FULLSCREEN BUTTON CLICKED: ", toggled_on)
+
 	if toggled_on:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 	else:
@@ -46,4 +48,4 @@ func _on_reset_button_pressed() -> void:
 
 
 func _on_back_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://main_menu.tscn")
+	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")

6371c057d56acdee0ad42097d38d5eebd2499f6c
nourtamerhamdyabbas-ux <nourtamerhamdyabbas@gmail.com>
2026-09-20 20:13:46 +0300

    finalizing the options scene

--- Scenes/options.gd
+++ Scenes/options.gd
@@ -1,11 +1,49 @@
 extends Control
 
 
-# Called when the node enters the scene tree for the first time.
+@onready var master_volume_slider = $OptionsPanel/Margins/Content/SettingsList/MasterVolumeSlider
+@onready var music_volume_slider = $OptionsPanel/Margins/Content/SettingsList/MusicVolumeSlider
+@onready var fullscreen_button = $OptionsPanel/Margins/Content/SettingsList/FullscreenButton
+@onready var sound_button = $OptionsPanel/Margins/Content/SettingsList/SoundButton
+@onready var reset_button = $OptionsPanel/Margins/Content/SettingsList/ResetButton
+@onready var back_button = $OptionsPanel/Margins/Content/SettingsList/BackButton
+
+
 func _ready() -> void:
-	pass # Replace with function body.
+	master_volume_slider.value = 80
+	music_volume_slider.value = 70
+	
+	fullscreen_button.button_pressed = false
+	sound_button.button_pressed = true
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_master_volume_slider_value_changed(value: float) -> void:
+	var volume_db = linear_to_db(value / 100.0)
+	AudioServer.set_bus_volume_db(0, volume_db)
+
+
+func _on_music_volume_slider_value_changed(value: float) -> void:
+	var volume_db = linear_to_db(value / 100.0)
+	AudioServer.set_bus_volume_db(0, volume_db)
+
+
+func _on_fullscreen_button_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+
+
+func _on_sound_button_toggled(toggled_on: bool) -> void:
+	AudioServer.set_bus_mute(0, not toggled_on)
+
+
+func _on_reset_button_pressed() -> void:
+	master_volume_slider.value = 80
+	music_volume_slider.value = 70
+	fullscreen_button.button_pressed = false
+	sound_button.button_pressed = true
+
+
+func _on_back_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://main_menu.tscn")

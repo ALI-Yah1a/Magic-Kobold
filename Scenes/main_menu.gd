c47573c69da0df586ac5dbfd8497f4adeb6fd85b
nourtamerhamdyabbas-ux <nourtamerhamdyabbas@gmail.com>
2026-09-21 06:24:36 +0300

    finalizing the how to play scene

--- Scenes/main_menu.gd
+++ Scenes/main_menu.gd
@@ -21,3 +21,7 @@ func _on_options_button_pressed() -> void:
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_how_to_play_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/how_to_play.tscn")
